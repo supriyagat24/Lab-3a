135cb0adfe465b868e6eb1f16eacfe60e194a104
supriya HS <supriya.gat24@gmail.com>
2026-03-16 09:56:00 +0530

    search operation done

--- stack.c
+++ stack.c
@@ -7,7 +7,7 @@ int top = -1;
 void push()
 {
     int item;
-    
+
     if(top == MAX-1)
     {
         printf("Stack Overflow\n");
@@ -29,7 +29,7 @@ void pop()
     }
     else
     {
-        printf("Deleted element is %d\n", stack[top]);
+        printf("Deleted element: %d\n", stack[top]);
         top--;
     }
 }
@@ -37,7 +37,7 @@ void pop()
 void display()
 {
     int i;
-    
+
     if(top == -1)
     {
         printf("Stack is Empty\n");
@@ -52,26 +52,62 @@ void display()
     }
 }
 
+void search()
+{
+    int item, i, flag = 0;
+
+    if(top == -1)
+    {
+        printf("Stack is Empty\n");
+        return;
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d",&item);
+
+    for(i = 0; i <= top; i++)
+    {
+        if(stack[i] == item)
+        {
+            printf("Element found at position %d\n", i);
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 0)
+    {
+        printf("Element not found\n");
+    }
+}
+
 int main()
 {
     int choice;
 
     while(1)
     {
-        printf("\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
-        printf("Enter choice: ");
+        printf("\n1.Push\n2.Pop\n3.Display\n4.Search\n5.Exit\n");
+        printf("Enter your choice: ");
         scanf("%d",&choice);
 
         switch(choice)
         {
             case 1: push();
                     break;
+
             case 2: pop();
                     break;
+
             case 3: display();
                     break;
-            case 4: return 0;
-            default: printf("Invalid Choice\n");
+
+            case 4: search();
+                    break;
+
+            case 5: return 0;
+
+            default: printf("Invalid choice\n");
         }
     }
 }
